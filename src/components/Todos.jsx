295ba1f723b16c0e94869976f3c95b0fbc1687ca
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-01-24 07:26:36 +0530

    feat: separate completed tasks from normal task
    - Implemented logic to filter and display completed tasks separately
    - Improved UX by visually separating completed tasks from normal tasks

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -10,28 +10,72 @@ function Todos() {
       <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
           <div className="mb-4">
-            <h1 className="text-grey-darkest">Todo List</h1>
+            <h1 className="font-semibold text-lg">Tasks</h1>
           </div>
-          {todos.map((todo) => (
-            <div className="flex mb-4 items-center" key={todo.id}>
-              <button
-                className="flex-no-shrink p-1 ml-2 mr-2 bg-transparent font-semibold rounded"
-                onClick={() => dispatch(completedTodo(todo.id))}
-              >
-                {todo.status ? <SquareCheck color="gray"/> : <Square />}
-              </button>
+          {todos
+            .filter((todo) => !todo.status)
+            .map((todo) => (
+              <div className="flex mb-4 items-center" key={todo.id}>
+                <button
+                  className="flex-no-shrink p-1 ml-2 mr-2 bg-transparent font-semibold rounded"
+                  onClick={() => dispatch(completedTodo(todo.id))}
+                >
+                  {todo.status ? <SquareCheck color="gray" /> : <Square />}
+                </button>
 
-              <p className={!todo.status?"w-full text-grey-darkest":"w-full text-gray-700 line-through"}>{todo.text}</p>
-              
-              <button
-                className="flex-no-shrink p-2 ml-2 border-2 bg-transparent hover:bg-red-500 text-red-700 
+                <p
+                  className={
+                    !todo.status
+                      ? "w-full text-grey-darkest"
+                      : "w-full text-gray-700 line-through"
+                  }
+                >
+                  {todo.text}
+                </p>
+
+                <button
+                  className="flex-no-shrink p-2 ml-2 border-2 bg-transparent hover:bg-red-500 text-red-700 
                 font-semibold hover:text-white py-2 px-4 border-red-500 hover:border-transparent rounded"
-                onClick={() => dispatch(removeTodo(todo.id))}
-              >
-                <Trash />
-              </button>
-            </div>
-          ))}
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                >
+                  <Trash />
+                </button>
+              </div>
+            ))}
+
+          <hr className="my-2" />
+          <h2 className="font-semibold text-lg mb-4 text-gray-600">Completed Tasks</h2>
+
+          {todos
+            .filter((todo) => todo.status)
+            .map((todo) => (
+              <div className="flex mb-4 items-center" key={todo.id}>
+                <button
+                  className="flex-no-shrink p-1 ml-2 mr-2 bg-transparent font-semibold rounded"
+                  onClick={() => dispatch(completedTodo(todo.id))}
+                >
+                  {todo.status ? <SquareCheck color="gray" /> : <Square />}
+                </button>
+
+                <p
+                  className={
+                    !todo.status
+                      ? "w-full text-grey-darkest"
+                      : "w-full text-gray-700 line-through"
+                  }
+                >
+                  {todo.text}
+                </p>
+
+                <button
+                  className="flex-no-shrink p-2 ml-2 border-2 bg-transparent hover:bg-red-500 text-red-700 
+                font-semibold hover:text-white py-2 px-4 border-red-500 hover:border-transparent rounded"
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                >
+                  <Trash />
+                </button>
+              </div>
+            ))}
         </div>
       </div>
     </>
