619b73c0b4fab2beaf8ee1cfd2c9c8837d530cab
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-02-12 22:12:26 +0530

    feat: add tooltip for sidebar trigger

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,6 +6,12 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Outlet } from "react-router";
 
 export default function Page() {
@@ -15,8 +21,22 @@ export default function Page() {
         <AppSidebar />
         <SidebarInset>
           <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
-            <SidebarTrigger className="-ml-1" />
-            <ModeToggle className="" />
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <SidebarTrigger className="-ml-1" />
+                </TooltipTrigger>
+                <TooltipContent side="right">
+                  <p>
+                    Open/close sidebar{" "}
+                    <span className="relative bg-muted px-[0.3rem] py-[0.1rem] rounded text-sm font-semibold">
+                      M
+                    </span>
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+              <ModeToggle />
+            </TooltipProvider>
           </header>
           <Outlet />
         </SidebarInset>
