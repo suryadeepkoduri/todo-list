1ea4f7d45305a81697ed7e65abed560e877c8af9
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-02-13 22:41:54 +0530

    edit Input style

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -11,8 +11,18 @@ import {
 } from "@/components/ui/sidebar";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "./ui/dialog";
 import { NavLink } from "react-router";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 export function AppSidebar({ ...props }) {
   return (
@@ -37,53 +47,17 @@ export function AppSidebar({ ...props }) {
             </DialogTrigger>
 
             <DialogContent>
-              <div className="flex align-middle justify-center m-auto">
-                <div className="flex flex-col items-center">
-                  <svg
-                    fill="#000000"
-                    version="1.1"
-                    id="Capa_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 32.811 32.811"
-                    xmlSpace="preserve"
-                    height="200"
-                    className="dark:fill-white"
-                  >
-                    <g>
-                      <path
-                        d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281
-		l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66
-		C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785
-		L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006
-		l5.646-10.576L32.149,16.811z"
-                      />
-                      <path
-                        d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281
-		l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66
-		C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785
-		L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006
-		l5.646-10.576L32.149,16.811z"
-                      />
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                      <g></g>
-                    </g>
-                  </svg>
-                  <p className="text-3xl font-semibold">Coming Soon!! </p>
-                </div>
-              </div>
+              <DialogHeader>
+                <form>
+                  <div className="flex flex-col gap-3">
+                    <Input placeholder="Task"/>
+                    <Textarea placeholder="Description" />
+                  </div>
+                </form>
+              </DialogHeader>
+              <DialogFooter>
+                <Button>Add Task</Button>
+              </DialogFooter>
             </DialogContent>
           </Dialog>
 
